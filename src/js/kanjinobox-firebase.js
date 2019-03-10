8ef002148b447399b05d4bd521c6587ec67b8f2a
Elisha Saylon <shoisathot@gmail.com>
2019-03-11 02:00:05 +0800

    11/03/19 1:59 AM
    Trying to get provider data.

--- src/js/kanjinobox-firebase.js
+++ src/js/kanjinobox-firebase.js
@@ -44,10 +44,14 @@ function signInWithTwitter() {
 
     var user = firebase.auth().currentUser;
 
-    if (user) {
-        alert(user.displayName);
-    } else {
-    // No user is signed in.
+    if (user != null) {
+    user.providerData.forEach(function (profile) {
+        console.log("Sign-in provider: " + profile.providerId);
+        console.log("  Provider-specific UID: " + profile.uid);
+        console.log("  Name: " + profile.displayName);
+        console.log("  Email: " + profile.email);
+        console.log("  Photo URL: " + profile.photoURL);
+    });
     }
 }
 
