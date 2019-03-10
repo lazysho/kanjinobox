f97b79eca4627f519252aa0f7075e03623dfb8f3
Elisha Saylon <shoisathot@gmail.com>
2019-03-11 01:52:04 +0800

    11/03/19 1:51 AM
    Test twiiter user object.

--- src/js/kanjinobox-firebase.js
+++ src/js/kanjinobox-firebase.js
@@ -37,7 +37,7 @@ function signInWithTwitter() {
         var secret = result.credential.secret;
 
         var user = result.user;
-        alert(user.user_id);
+        alert(user.toString());
     }).catch(function(error) {
         alert(error.message);
     });
