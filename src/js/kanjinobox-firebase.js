7117c1372009d2ab91cb2e75ea539bedb73ed472
Elisha Saylon <shoisathot@gmail.com>
2019-03-12 00:49:07 +0800

    12/03/19 12:48 AM
    Testing twitter sign in function.

--- src/js/kanjinobox-firebase.js
+++ src/js/kanjinobox-firebase.js
@@ -73,20 +73,6 @@ function signInWithTwitter() {
         // show error
         console.log(error.message);
     });
-
-    var user = firebase.auth().currentUser;
-
-    if (user != null) {
-    user.providerData.forEach(function (profile) {
-        console.log("Sign-in provider: " + profile.providerId);
-        console.log("  Provider-specific UID: " + profile.uid);
-        console.log("  Name: " + profile.displayName);
-        console.log("  Email: " + profile.email);
-        console.log("  Photo URL: " + profile.photoURL);
-    });
-    } else {
-        alert("null");
-    }
 }
 
 function checkIfUserNameExists(username) {
