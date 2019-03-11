6ab7271846195187c632596991f4d9d1903a91a1
Elisha Saylon <shoisathot@gmail.com>
2019-03-12 01:29:36 +0800

    12/03/19 1:28 AM
    Fixing sign in with twitter, still won't create user document

--- src/js/kanjinobox-firebase.js
+++ src/js/kanjinobox-firebase.js
@@ -52,39 +52,58 @@ function addNewUser(emailStr, passwordStr) {
 }
 
 function signInWithTwitter() {
-    var provider = new firebase.auth.TwitterAuthProvider();
-
     if (firebase.auth().currentUser != null) {
-        window.location.href = "pages/setup.html"
+        var ref = kanjinoboxdb.collection("users").doc(currentUser.uid);
+        ref.get().then(function(doc) {
+            if (doc.exists) {
+                if (doc.firebaseConfig == "") {
+                    // redirect to database setup
+                    if (window.location.href != "index.html") {
+                        window.location.href = "pages/setup.html";
+                    }
+                } else {
+                    // redirect to dashboard
+                    console.log("To dashboard.");
+                }
+            } else {
+                // show error
+                console.log("User document doesn't exist.");
+            }
+        }).catch(function(error) {
+            console.log(error);
+        });
     } else {
+        var provider = new firebase.auth.TwitterAuthProvider();
         firebase.auth().signInWithPopup(provider).then(function(result) {
             // create user document and add to collection
             var twitterUser = result.user;
-            var firebaseUser = firebase.auth().currentUser;0
+            var firebaseUser = firebase.auth().currentUser;
             
-            kanjinoboxdb.collection("users").doc(firebaseUser.uid).set({
-                userId: firebaseUser.uid,
-                email: twitterUser.providerData.profile.email,
-                username: twitterUser.providerData.profile.displayName,
-                firebaseConfig: ""
-            }).catch(function(error) {
-                console.log(error.message);
-            });
+            if(!checkIfUserExists(firebaseUser.uid)) {
+                console.log("Creating user document....");
+                kanjinoboxdb.collection("users").doc(firebaseUser.uid).set({
+                    userId: firebaseUser.uid,
+                    email: twitterUser.providerData.profile.email,
+                    username: twitterUser.providerData.profile.displayName,
+                    firebaseConfig: ""
+                }).catch(function(error) {
+                    console.log(error.message);
+                });
+            }
+            
         }).catch(function(error) {
             // show error
             console.log(error.message);
         });
-        
-        window.location.href = "pages/setup.html"
     }
     
 }
 
-function checkIfUserNameExists(username) {
+function checkIfUserExists(userId) {
     // get all users
     kanjinoboxdb.collection("users").get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-            if(username == doc.data().username) {
+            if(userId == doc.data().userId) {
                 return true;
             }
         });
